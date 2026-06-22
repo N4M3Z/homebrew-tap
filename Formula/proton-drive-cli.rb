deb215a6a8adaa925be246bffa4cce6cfc8e78bb
Martin Zeman <martin.zeman@pm.me>
2026-06-22 23:35:04 +0200

    feat: extend proton-drive-cli to intel macOS and linux (arm64 + x64)

--- Formula/proton-drive-cli.rb
+++ Formula/proton-drive-cli.rb
@@ -12,6 +12,21 @@ class ProtonDriveCli < Formula
       url "https://proton.me/download/drive/cli/#{version}/darwin-arm64/proton-drive", using: :nounzip
       sha256 "7f579ff56aa57657f68f27ff70e322388874f57a52df6396524b14de763ce33c"
     end
+    on_intel do
+      url "https://proton.me/download/drive/cli/#{version}/darwin-x64/proton-drive", using: :nounzip
+      sha256 "647f56db3f0559b6ac8031a9fce95c46537827940d684a7524a90d4fcbb428ff"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://proton.me/download/drive/cli/#{version}/linux-arm64/proton-drive", using: :nounzip
+      sha256 "722e36559102dfb28d1897bff891961ea77ae62d63eee327bc6d132d41a2c217"
+    end
+    on_intel do
+      url "https://proton.me/download/drive/cli/#{version}/linux-x64/proton-drive", using: :nounzip
+      sha256 "89a54131a0811e42ea18ec43073d6eb347d80f594ed0226009bb94118f4cda86"
+    end
   end
 
   def install
